52224e08c88278f16f61b3e0adaec11c8ce574a5
And4Web <anands2807@gmail.com>
2022-12-30 20:28:00 +0530

    simple server created

---
+++ project1/basicServer.js
@@ -0,0 +1,11 @@
+const http = require('http');
+
+const hostname = '127.0.0.1';
+const port = 5500;
+
+const server = http.createServer((req, res)=>{
+  res.writeHead(200, {'Content-type': "application/json"});
+  res.end('Hello, Anand!');
+})
+
+server.listen(port, hostname, ()=>console.log(`server running at Port: ${port} and host: ${hostname}}`))
