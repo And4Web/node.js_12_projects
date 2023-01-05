1569bbe48c273d0cb7c4381ff53288ae537007bf
And4Web <anands2807@gmail.com>
2023-01-05 17:11:38 +0530

    pug issue

--- project2_ExpressWebsite/server.js
+++ project2_ExpressWebsite/server.js
@@ -8,13 +8,18 @@ require('dotenv').config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
 app.get('/', (req, res)=>{
-  res.send('Hello! This server is running...')
+  // res.send('Hello! This server is running...')
+  res.render('index');
 })
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}!`);
+  // console.log(path.join(__dirname, 'views'))
 });
