a7ec0fd846eacd9aa65f480da00c034a901c7eac
And4Web <anands2807@gmail.com>
2023-01-06 15:29:35 +0530

    html to pug converted bootstrap example page

--- project2_ExpressWebsite/server.js
+++ project2_ExpressWebsite/server.js
@@ -2,37 +2,36 @@ const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
 const pug = require('pug');
-// const hbs = require('hbs');
 const nodemailer = require('nodemailer');
+
 require('dotenv').config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
 //dynamic
 app.set('views', path.join(__dirname, '/views'));
 app.set('view engine', 'pug');
-// app.set('view engine', hbs);
-// app.use('views', path.join(__dirname, 'views'))
+
 app.get("/", (req, res)=>{
   res.render('index', {
     title: 'Pug'
-  })
+  });
+  // console.log(req.url);
 })
 
 // Static
 const static = path.join(__dirname, './public');
 app.use(express.static(static));
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
-
 app.get('/', (req, res)=>{
   res.send('Hello! This server is running...')
-  // res.render('index');
 })
 
 app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}! - ${path.join(__dirname, '/views')}`);
+  console.log(`App listening on port ${PORT}!`);
   // console.log(path.join(__dirname, 'views'))
 });
