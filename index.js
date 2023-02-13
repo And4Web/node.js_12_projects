d444c6aa2e36ddf6739fb795f8b51d0a5333f0f0
And4Web <anands2807@gmail.com>
2023-02-13 18:57:29 +0530

    pug layout pages added

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
+// require('./project1');
+// require('./project2');
 require('./project3_authentication/server');
+// require('./project4');
+// require('./project5');
+// require('./project6');
+// require('./project7');
