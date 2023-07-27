d1e67074b9c82f411fa58abe83ce0df2bfb3ba00
unknown <anands2807@gmail.com>
2023-07-27 19:00:40 +0530

    changes to project 2

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
+// const path = require("path");
 // require('./project1/basicServer.js');
 // require('./project1/serverWithHTML.js');
-require('./project2_ExpressWebsite/server.js');
-// require('./project3_authentication/server');
+// require('./project2_ExpressWebsite/server.js');
+require('./project3_authentication/server');
 // require('./project4');
 // require('./project5');
 // require('./project6');
 // require('./project7');
+
